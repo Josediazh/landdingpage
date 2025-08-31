0cd7a034062f6149140bd9d7187d109240900903
jose diaz <josediazh1925@gmail.com>
2025-08-30 19:58:13 -0600

    ajustes en el video tamaño de logo y la galeria

--- templates/tpl-home.php
+++ templates/tpl-home.php
@@ -33,12 +33,12 @@ $query_testimonial = new WP_Query($args_testimonial);
             endwhile;
         endif;
         ?>
-        <video controls src="<?php echo $video; ?>"></video>
-        <div class="btnapply"><a target="_blank" href="http://localhost/landingpage/?fluent-form=3">Apply today</a></div>
+        <video poster="<?php echo get_template_directory_uri().'/img/portadavideoTransformbody.png'?>" controls src="<?php echo $video; ?>"></video>
+        <div class="btnapply"><a target="_blank" href="https://teamtransforms.com/?fluent-form=3">APPLY TODAY</a></div>
     </section>
     <section class="gallerysec">
         <h3>TRUE STORIES.</h3>
-        <h3>POWERFULE CHANGES.</h3>
+        <h3>POWERFUL CHANGES.</h3>
         <h3 class="blue">REAL RESULTS.</h3>
         <div class="gallerycontainer grid-x">
         <?php
@@ -58,13 +58,13 @@ $query_testimonial = new WP_Query($args_testimonial);
             <?php echo do_shortcode(wpautop($unlocktext)); ?>
         </div>
         <div class="content grid-x">
-            <div class="cell large-3 block">
-                <img src="<?php echo get_template_directory_uri().'/img/banner.jpeg' ?>" alt="">
-            </div>
-            <div class="cell large-9 block">
+            <div class="cell large-6 block">
                 <h1>TRANSFORM YOUR LIFE</h1>
                 <h3>FOR GUARANTEED RESULTS</h3>
                 <p>Build lifelong habits and unlock your genetic potential at this exclusive member price.</p>
+            </div>
+            <div class="cell large-6 block">
+                <img src="<?php echo get_template_directory_uri().'/img/banner.jpeg' ?>" alt="">
             </div>
         </div>
     </section>
@@ -77,7 +77,7 @@ $query_testimonial = new WP_Query($args_testimonial);
         <div class="content grid-x">
             <?php echo do_shortcode(wpautop($whatunlock)); ?>
         </div>
-        <div class="btnapply"><a target="_blank" href="http://localhost/landingpage/?fluent-form=3">Apply today</a></div>
+        <div class="btnapply"><a target="_blank" href="https://teamtransforms.com/?fluent-form=3">APPLY TODAY</a></div>
     </section>
     <section class="applytoday">
         <h1>Stop Wasting Time.</h1>
@@ -86,7 +86,7 @@ $query_testimonial = new WP_Query($args_testimonial);
         <h1>Follow the Path.</h1>
         <h1>Step Into Your God-Given Potential.</h1>
         <h1>Transform Your Life.</h1>
-        <div class="btnapply"><a target="_blank" href="http://localhost/landingpage/?fluent-form=3">Apply today</a></div>
+        <div class="btnapply"><a target="_blank" href="https://teamtransforms.com/?fluent-form=3">APPLY TODAY</a></div>
     </section>
     <section class="testimonials grid-x">
         <?php
@@ -108,10 +108,11 @@ $query_testimonial = new WP_Query($args_testimonial);
         <div class="content grid-x align-middle align-center">
             <div class="cell large-8">
                 <h3>Are you ready to commit to your journey?</h3>
-                <p>In life, there are those who sit back and let life happen, and those who take charge and create the life they want.At Project 8, we invite you to step into the ring, take ownership, and make the decision to become your best self.Join us, and let's begin your journey to greatness together.</p>
+                <p>In life, some sit back and let the years pass, while others rise up and create the life they were meant to live. At Team Transforms, we call you to step forward, take ownership, and commit to becoming the strongest version of yourself—physically, mentally, and spiritually. With my daily accountability, proven system, and the teachings of Jesus Christ at the center, you’ll have the unwavering support to break free from the old you and step boldly into the man you were created to be.</p>
             </div>
             <div class="cell large-4">
-                <div class="btnapply"><a target="_blank" href="http://localhost/landingpage/?fluent-form=3">Apply today</a></div>
+                <img src="<?php echo get_template_directory_uri().'/img/sergiotransform.webp' ?>" alt="">
+                <div class="btnapply"><a target="_blank" href="https://teamtransforms.com/?fluent-form=3">APPLY TODAY</a></div>
             </div>
         </div>
     </section>
@@ -123,8 +124,8 @@ $query_testimonial = new WP_Query($args_testimonial);
             endwhile;
         endif;
         ?>
-        <video controls src="<?php echo $video; ?>"></video>
-        <div class="btnapply"><a target="_blank" href="http://localhost/landingpage/?fluent-form=3">Apply today</a></div>
+        <video poster="<?php echo get_template_directory_uri().'/img/portadavideoTransformbody.png'?>" controls src="<?php echo $video; ?>"></video>
+        <div class="btnapply"><a target="_blank" href="https://teamtransforms.com/?fluent-form=3">APPLY TODAY</a></div>
     </section>
 </div>
 
